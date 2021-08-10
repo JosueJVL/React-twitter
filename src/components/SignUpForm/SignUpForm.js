5e54605b69e667ab87c5261240df5c0f7e7e8d00
josue <jvl_josue@hotmail.com>
2021-08-10 14:44:14 -0500

    Se guardan los datos del formulario en el registro de estado

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,54 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Form, Button, Spinner } from "react-bootstrap";
 
 import "./SignUpForm.scss";
 
 export default function SignUpForm(props) {
-
     const { setShowModal } = props;
-    console.log(SignUpForm)
+
+    const [formData, setFormData] = useState(initialFormValue)
 
     const onSubmit = e => {
         e.preventDefault();
-        setShowModal = false;
-        
+        console.log(formData)
+    };
+
+    //Solo sirve para formularios que estan construidos con puros input
+    const onChange = e => {
+        setFormData({ ...formData, [e.target.name] : e.target.value})
     }
 
     return (
         <div className="sign-up-form">
             <h2>Crea tu cuenta</h2>
-            <form onSubmit={onSubmit}>
-
+            <Form onSubmit={onSubmit} onChange={onChange}>
                 <Form.Group>
                     <Row>
                         <Col>
-                        <Form.Control type="text" placeholder="Nombre" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Nombre"
+                                name="nombre"
+                                defaultValue={formData.nombre}
+                                // onChange={e =>
+                                //     setFormData({ ...formData, nombre: e.target.value})}
+                            />
                         </Col>
                         <Col>
-                        <Form.Control type="text" placeholder="Apellidos" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Apellidos"
+                                name="apellidos"
+                                defaultValue={formData.apellidos}
+                            />
+                        </Col>
+                    </Row>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Control
+                        type="email"
+                        placeholder="Correo electronico"
+                        name="email"
+                        defaultValue={formData.email}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Row>
+                        <Col>
+                            <Form.Control
+                                type="password"
+                                placeholder="Contraseña"
+                                name="password"
+                                defaultValue={formData.password}
+                            />
+                        </Col>
+                        <Col>
+                            <Form.Control
+                                type="password"
+                                placeholder="Repetir contraseña"
+                                name="confirmPassword"
+                                defaultValue={formData.confirmPassword}
+                            />
                         </Col>
                     </Row>
                 </Form.Group>
 
-                <Form.Group>
-                    <Form.Control type="email" placeholder="Correo Electronico" />
-                </Form.Group>
-
-                <Form.Group>
-                    <Row>
-                        <Col>
-                        <Form.Control type="password" placeholder="Contrasena" />
-                        </Col>
-                        <Col>
-                        <Form.Control type="password" placeholder="Repetir Contrasena" />
-                        </Col>
-                    </Row>
-                </Form.Group>
-
-                <Button variante="primary" type="submit" >
-                    Registrate
+                <Button variant="primary" type="submit">
+                     Registrase
                 </Button>
-            </form>
+            </Form>
         </div>
     )
+}
+
+
+function initialFormValue(){
+    return {
+        nombre: "",
+        apellidos: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    }
 }
