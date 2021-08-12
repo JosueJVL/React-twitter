3465e1d960e99b5c873755642db7622771828148
Josue Vicencio Lara <jvl_josue@hotmail.com>
2021-08-12 16:26:57 -0500

    Estructura de la pagina de Usuarios

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import {Button, Spinner} from 'react-bootstrap';
+import BasicLayout from '../../layout/BasicLayout/BasicLayout';
+
+
 
 import "./User.scss";
 
 export default function User() {
     return (
-        <div>
-            <h1>Usersss</h1>
-        </div>
+        <BasicLayout className="user">
+            <div className="user__title">
+                <h2>
+                Josue Vicencio Lara
+                </h2>
+            </div>
+            <div>
+                Banner Ususario
+            </div>
+            <div>
+                Informacion Usuario
+            </div>
+            <div className="user__twittws">
+                Lista de mensajes
+            </div>
+        </BasicLayout>
     )
 }
